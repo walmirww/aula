2ad9f0b31c023c7653aeda000dca908f37caf5b3
Walmir Wild <walmirww@gmail.com>
2024-10-15 08:33:46 -0300

    first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { CarroslistComponent } from './components/carroslist/carroslist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: "carros", pathMatch: "full"},
+    {path: "carros", component: CarroslistComponent}
+];
